6d2131202a774bcbd321482c26edd7c3c432420a
Mehul Kar <Mehul Kar>
2023-12-14 16:23:10 -0600

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,61 @@
-use std::sync::{Arc, Mutex};
 use std::vec;
 
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::process::{Child, Command};
+use tokio::sync::Mutex;
 
-pub struct Foo {
-    pub lock: Arc<Mutex<i32>>,
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+    let mut task_zero = Command::new("cat")
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("stdin manager start");
+
+    let t0_stdout = task_zero.stdout.take().expect("mgr stdout");
+    tokio::spawn(read_child_output(BufReader::new(t0_stdout), "mgr"));
+    let mutex = Mutex::new(0);
+    let t1 = spawn_task("t1", "npm", vec!["run", "build-a"], &mutex);
+    println!("spawning task 2!");
+    let t2 = spawn_task("t2", "npm", vec!["run", "build-b"], &mutex);
+
+    // Use Tokio to read lines from stdin and send them to the stdin manager
+    let mut t0_stdin = task_zero.stdin.take().expect("stdin mgr stdin");
+
+    let mut in_reader = io::BufReader::new(io::stdin()).lines();
+    while let Some(line) = in_reader.next_line().await.unwrap() {
+        t0_stdin.write_all(line.as_bytes()).await.expect("");
+        t0_stdin.write_all(b"\n").await.expect("");
+    }
+
+    // TODO: Wait for both asynchronously instead of blocking like this
+    println!("done");
 }
 
-impl Foo {
-    pub fn new() -> Foo {
-        Foo {
-            lock: Arc::new(Mutex::new(0)),
-        }
-    }
+fn spawn_task(name: &str, program: &str, args: Vec<&str>, mutex: &Mutex<i32>) -> Child {
+    println!("about to lock the mutex: {}", name);
+    let _guard = mutex.lock();
 
-    pub async fn start(&self) {
-        let mut stdin_mgr = Command::new("cat")
-            .stdin(std::process::Stdio::piped())
-            .stdout(std::process::Stdio::piped())
-            .spawn()
-            .expect("stdin manager start");
+    println!("spawn_task: {} has the lock!", name);
 
-        let mut task_one = self.spawn_task("npm", vec!["run", "start"]);
-        let mut task_two = self.spawn_task("npm", vec!["run", "dev"]);
+    let mut cmd = Command::new(program)
+        .args(args.clone())
+        .stdout(std::process::Stdio::piped())
+        .stdin(std::process::Stdio::null())
+        .spawn()
+        .expect(format!("{} {:?} to start", program, args.clone()).as_str());
 
-        // Read and print stdout
-        let t1_stdout = task_one.stdout.take().expect("t1 stdout");
-        let t2_stdout = task_two.stdout.take().expect("t2 stdout");
-        let mgr_stdout = stdin_mgr.stdout.take().expect("mgr stdout");
-        tokio::spawn(read_child_output(BufReader::new(t1_stdout), "t1"));
-        tokio::spawn(read_child_output(BufReader::new(t2_stdout), "t2"));
-        tokio::spawn(read_child_output(BufReader::new(mgr_stdout), "mgr"));
+    let stdout = cmd
+        .stdout
+        .take()
+        .expect(format!("{} stdout", name).as_str());
 
-        // Use Tokio to read lines from stdin and send them to the stdin manager
-        let mut stdin_mgr_stdin = stdin_mgr.stdin.take().expect("stdin mgr stdin");
-        let mut in_reader = io::BufReader::new(io::stdin()).lines();
-        while let Some(line) = in_reader.next_line().await.unwrap() {
-            stdin_mgr_stdin.write_all(line.as_bytes()).await.expect("");
-            stdin_mgr_stdin.write_all(b"\n").await.expect("");
-        }
-    }
+    println!("reading child ouptut {}!", name);
 
-    fn spawn_task(&self, program: &str, args: Vec<&str>) -> Child {
-        Command::new(program)
-            .args(args.clone())
-            .stdout(std::process::Stdio::piped())
-            .stdin(std::process::Stdio::null())
-            .spawn()
-            .expect(format!("{} {:?} to start", program, args.clone()).as_str())
-    }
+    tokio::spawn(read_child_output(BufReader::new(stdout), "t1"));
+
+    cmd
 }
 
 async fn read_child_output(mut reader: BufReader<tokio::process::ChildStdout>, prefix: &str) {
@@ -62,10 +68,4 @@ async fn read_child_output(mut reader: BufReader<tokio::process::ChildStdout>, p
         print!("{}: {}", prefix, line);
         line.clear();
     }
-}
-
-#[tokio::main]
-async fn main() {
-    let foo = Foo::new();
-    foo.start().await;
 }
