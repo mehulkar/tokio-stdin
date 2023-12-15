ce7886dd1e112e28f58d4e36708f0bef1c32e98d
Mehul Kar <Mehul Kar>
2023-12-14 21:42:13 -0600

    try to mutex

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,63 @@
+use std::process::Stdio;
 use std::vec;
 
 use tokio::io::{self, AsyncBufReadExt, AsyncWriteExt, BufReader};
-use tokio::process::{Child, Command};
+use tokio::process::{Child, ChildStdin, Command};
 use tokio::sync::Mutex;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    let mut task_zero = Command::new("cat")
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
+    let mut t0 = Command::new("cat")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
         .spawn()
         .expect("stdin manager start");
 
-    let t0_stdout = task_zero.stdout.take().expect("mgr stdout");
+    let t0_stdout = t0.stdout.take().expect("mgr stdout");
+    let mut t0_stdin = t0.stdin.take().expect("stdin mgr stdin");
+
     tokio::spawn(read_child_output(BufReader::new(t0_stdout), "mgr"));
-    let mutex = Mutex::new(0);
-    let t1 = spawn_task("t1", "npm", vec!["run", "build-a"], &mutex);
-    println!("spawning task 2!");
-    let t2 = spawn_task("t2", "npm", vec!["run", "build-b"], &mutex);
 
-    // Use Tokio to read lines from stdin and send them to the stdin manager
-    let mut t0_stdin = task_zero.stdin.take().expect("stdin mgr stdin");
+    // create a mutex guard around the stdin manager's stdin stream
+    let mutex = Mutex::new(&t0_stdin);
+    let mut t1 = spawn_task("t1", "npm", vec!["run", "build-a"], &mutex);
+    let mut t2 = spawn_task("t2", "npm", vec!["run", "build-b"], &mutex);
 
+    // Read lines from stdin and send them to the stdin manager
     let mut in_reader = io::BufReader::new(io::stdin()).lines();
     while let Some(line) = in_reader.next_line().await.unwrap() {
         t0_stdin.write_all(line.as_bytes()).await.expect("");
         t0_stdin.write_all(b"\n").await.expect("");
     }
 
-    // TODO: Wait for both asynchronously instead of blocking like this
-    println!("done");
+    t0.wait().await.unwrap();
+    t1.await.wait().await.unwrap();
+    t2.await.wait().await.unwrap();
 }
 
-fn spawn_task(name: &str, program: &str, args: Vec<&str>, mutex: &Mutex<i32>) -> Child {
-    println!("about to lock the mutex: {}", name);
-    let _guard = mutex.lock();
+async fn spawn_task(
+    name: &str,
+    program: &str,
+    args: Vec<&str>,
+    mutex: &Mutex<&ChildStdin>,
+) -> Child {
+    let locked_stdin = mutex.lock().await;
+    let xx = *locked_stdin;
+    let yy = *xx;
 
     println!("spawn_task: {} has the lock!", name);
 
     let mut cmd = Command::new(program)
         .args(args.clone())
-        .stdout(std::process::Stdio::piped())
-        .stdin(std::process::Stdio::null())
+        .stdout(Stdio::piped())
+        .stdin(yy)
         .spawn()
-        .expect(format!("{} {:?} to start", program, args.clone()).as_str());
+        .expect(format!("{} to start", name).as_str());
 
     let stdout = cmd
         .stdout
         .take()
         .expect(format!("{} stdout", name).as_str());
-
-    println!("reading child ouptut {}!", name);
 
     tokio::spawn(read_child_output(BufReader::new(stdout), "t1"));
 
